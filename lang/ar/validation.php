a083e8d66a408642e4c2627ed712d07fc9a25e86
Abdu-Azaz <abderrahmane.azaz@edu.uiz.ac.ma>
2024-05-12 18:13:34 +0100

    commiting

---
+++ lang/ar/validation.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'required' => 'ضروري :attribute  الحقل'
+];
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,33 +1,32 @@
 <?php
 
 return [
-    'deans-word' =>
-    "La Faculté des Sciences
-    Appliquées-Ait Melloul de l'Université Ibn Zohr, FSA, est le premier établissement marocain dédié
+    'deans-word' => 'La Faculté des Sciences
+    Appliquées-Ait Melloul de l\'Université Ibn Zohr, FSA, est le premier établissement marocain dédié
     aux sciences appliquées et leurs interactions socio-économiques. Une particularité qui inscrit notre
-    projet de développement dans le cadre de la vision stratégique de l'enseignement supérieur au Maroc.
+    projet de développement dans le cadre de la vision stratégique de l\'enseignement supérieur au Maroc.
     Crée par le bulletin officiel N° 6789 de 24 Juin 2019 (20 Choual 1440), la FSA vise à répondre aux
-    besoins et aux exigences du marché de l'emploi à l'échelle régionale et à l'échelle nationale. <br>
-    La Faculté des Sciences Appliquées, pilier du grand Campus Universitaire d'Ait Melloul, demeure une
-    source d'enseignements, de formations, de recherche scientifique et de l'innovation. En effet, une
+    besoins et aux exigences du marché de l\'emploi à l\'échelle régionale et à l\'échelle nationale. <br>
+    La Faculté des Sciences Appliquées, pilier du grand Campus Universitaire d\'Ait Melloul, demeure une
+    source d\'enseignements, de formations, de recherche scientifique et de l\'innovation. En effet, une
     panoplie de filières scientifiques et techniques issues de la Physique Appliquée, de la Chimie
-    Appliquées, des Sciences de Vie et de l'Informatique.<br><br>Vu son emplacement géographique
-    stratégique au sud du Maroc, la Faculté des Sciences Appliquées participe à la création d'une
+    Appliquées, des Sciences de Vie et de l\'Informatique.<br><br>Vu son emplacement géographique
+    stratégique au sud du Maroc, la Faculté des Sciences Appliquées participe à la création d\'une
     synergie active et à la promotion de la formation appliquée et académique en intégrant toutes les
     parties prenantes et tous les opérateurs concernés. Pertinence, Excellence, Créativité, Polyvalence
     et Endurance sont les cinq devises de la jeune Faculté pour assurer un processus de qualité sur les
     niveaux académique, scientifique, administratif et gestionnaire.<br><br>Sur le plan académique, le
     staff administratif et pédagogique de la faculté se dévoue sans compter pour garantir un
-    enseignement et/ou un encadrement de haute gamme. \"Un étudiant épanoui est un bon futur citoyen\"
-    reste la mission de la FSA qui propose plusieurs actions d'accompagnement et favorise le
+    enseignement et/ou un encadrement de haute gamme. "Un étudiant épanoui est un bon futur citoyen"
+    reste la mission de la FSA qui propose plusieurs actions d\'accompagnement et favorise le
     renforcement de la personnalité estudiantine via la promotion des Sofs-Skills et des ouvertures
     socio-cultuelles. La FSA focalise ses efforts et oriente ses ressources afin de mettre en œuvre les
     grands chantiers initiés par le Ministère de tutelle à savoir la promotion de la pédagogie
-    numérique, de la recherche scientifique et de l'innovation.<br><br> Un des futurs challenges de la
-    FSA serait de créer /multiplier les partenariats et de renforcer les coopérations. C'est ainsi que
-    la FSA entend se maintenir dans la ligne de l'excellence tracée par ses fondateurs pour remplir
+    numérique, de la recherche scientifique et de l\'innovation.<br><br> Un des futurs challenges de la
+    FSA serait de créer /multiplier les partenariats et de renforcer les coopérations. C\'est ainsi que
+    la FSA entend se maintenir dans la ligne de l\'excellence tracée par ses fondateurs pour remplir
     pleinement sa responsabilité sociale envers la région Souss-Massa et le Maroc tout entier.`<br><br>
     Le Doyen, le corps professoral et administratif et les étudiants de la Faculté des Sciences
     Appliquées ont le plaisir de participer au rayonnement universitaire marocain. Bienvenue à la
-    Faculté des Sciences Appliquées… Bienvenue à la Faculté de Demain"
+    Faculté des Sciences Appliquées… Bienvenue à la Faculté de Demain'
 ];
